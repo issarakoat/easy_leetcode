5d07dc120d32f764ca1e71a349b54a2254259719
issarakoat <weeravut.issarakoat@gmail.com>
2020-01-16 10:25:35 -0700

    finished 35

--- Leetcode/LeetcodeClass.cs
+++ Leetcode/LeetcodeClass.cs
@@ -113,5 +113,15 @@ namespace Leetcode
             double res = Math.Sqrt((me_p - me_o) * (me_p - me_o)/N);
             return res;
         }
+        public int SearchInsertPosition_35(int[] nums, int target)
+        {
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (target == nums[i]) return i;
+                if (target > nums[i]) index = i+1;
+            }
+            return index;
+        }
     } 
 }
